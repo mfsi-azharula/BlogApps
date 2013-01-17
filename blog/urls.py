0872a06ce1609cd05dce0e338e1414373cc59a13
mfsi-azharula <azharula@mindfiresolutions.com>
2013-01-17 16:59:30 +0530

    Added comment and modified indentation

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,16 @@
+"""
+    @author:Mindfire
+    @dateCreated:
+    url container file  which contaiins all the url variables of blog apps.
+"""
+
 from django.conf.urls.defaults import *
 from blog.models import *
 from django.core.urlresolvers import reverse
 from blog import views
 
 urlpatterns = patterns('',
+
    url(r"^(\d+)/$", views.post, name='post'),
    url(r"^add_comment/(\d+)/$", views.add_comment, name='add_comment'),
    url(r"^delete_comment/(\d+)/$", views.delete_comment, name='delete_comment'),
