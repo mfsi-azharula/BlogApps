a4fc4d224a574afa264de61c1630be636bfc7928
mfsi-azharula <azharula@mindfiresolutions.com>
2013-01-17 17:01:59 +0530

    Added new comment

--- urls.py
+++ urls.py
@@ -1,14 +1,21 @@
+"""
+    @author:Mindfire
+    @dateCreated:
+    Main url continer details file.
+"""
+
+import os.path
 from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-import os.path
+
 #admin.autodiscover()
 
 urlpatterns = patterns('',
-    
+
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^', include('blog.urls', namespace="blog")),
+    url(r'^', include('blog.urls', namespace="blog")),
 	( r'^static/(?P<path>.*)$', 'django.views.static.serve', 
     { 'document_root': os.path.join(os.path.dirname(__file__), 'media').replace('\\','/') } ),)
